72822350c0d69db3bba7d7e9af8f839650542f1f
Miles Cragg <milescragg@gmail.com>
2022-09-14 16:36:00 +0100

    Phase 2.3 completed

--- spec/todo_list_spec.rb
+++ spec/todo_list_spec.rb
@@ -10,6 +10,12 @@ describe 'todo_list checks a string for "#TODO"' do
   it 'returns true if contains "#TODO"' do
     expect(todo_checker("#TODO")).to eq (true)
   end
+  it 'returns false if contains "#todo"' do
+    expect(todo_checker("#todo or not #todo")).to eq (false)
+  end
+  it 'returns true if contains multiple iterations of "#TODO"' do
+    expect(todo_checker("#TODO or not #TODO")).to eq (true)
+  end
 end
   
   
